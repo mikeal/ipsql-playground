9f8e02e96ab534897bc13a25236edafa0032f407
Mikeal Rogers <mikeal.rogers@gmail.com>
2021-02-04 23:05:25 +0000

    fix: docs and fixes

--- playground.svelte
+++ playground.svelte
@@ -100,4 +100,66 @@ error {
      <Content>Example: <code>CREATE TABLE People (Age INT, Name: VARCHAR(256))<code></Content>
   </Paper>
 
+  <Paper square>
+    <Title>Supported Grammar</Title>
+<Content>
+<pre>
+- CREATE
+  - [ ] DATABASE (it isn't clear how we would want to implement and support this yet)
+  - TABLE
+    - [x] INTEGER
+    - [x] VARCHAR(size)
+    - [x] FLOAT
+    - [ ] DATE
+    - [ ] TIME
+    - [ ] DATETIME
+    - [ ] BLOB (should use the FBL, values smaller than 32b will be inline binary, below 1MB should
+                be a single raw block link, anything else is a full tree FBL as a stream)
+    - [ ] TEXT (may never support, according to spec this is upt 2GB of string data so it's hard
+                to figure out what the inline vs linking rules would be. Instead, using VARCHAR
+                as the inlined string and BLOB the *probably* linked type)
+    - [ ] BOOLEAN (this isn't actually supported in the SQL parser we use, must be quite rare)
+- [ ] ALTER TABLE
+- INSERT
+  - [x] ROWS `INSERT INTO table_name VALUES ( 'test' )`
+  - [x] COLUMNS `INSERT INTO table_name ( column_name ) VALUES ( 'test' )`
+- UPDATE
+  - [x] UPDATE SET w/o WHERE
+  - [x] UPDATE SET WHERE
+- DELETE
+  - [ ] DELETE w/o WHERE (deletes table)
+  - [ ] DELETE WHERE
+- SELECT
+  - [x] * `SELECT * FROM table_name`
+  - [x] COLUMNS `SELECT column1, column2 FROM table_name`
+  - [ ] ROWID meta column
+  - [ ] TOP
+  - [x] MIN
+  - [x] MAX
+  - [x] COUNT
+  - [x] AVG
+  - [x] SUM
+  - [ ] LIKE *pattern*
+  - WHERE
+    - [x] AND
+    - [x] OR
+    - [x] ASC, DESC
+    - [x] COMPARISON OPERATORS
+    - [ ] GROUP BY
+    - [x] ORDER BY
+    - [ ] BETWEEN
+    - [ ] LIKE
+    - [ ] IN
+    - [ ] NOT
+    - [ ] IS NOT NULL, IS NULL
+  - [ ] JOIN
+  - [x] ORDER BY
+  - [ ] GROUP BY
+  - [ ] HAVING
+  - [ ] UNION
+</pre>
+</Content>
+
+  </Paper>
+
 </ipsql-docs>
